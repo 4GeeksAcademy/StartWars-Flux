dbad75da9303178bbbb7bc3a3561aa5a1b44f753
Luis Miguel Del Valle <150975497+lumi-tip@users.noreply.github.com>
2024-03-09 23:01:34 +0000

    :lipstick: adding some css to the navbar

--- src/js/component/navbar.js
+++ src/js/component/navbar.js
@@ -2,18 +2,22 @@ import React from "react";
 import { Link } from "react-router-dom";
 import { useEffect, useContext } from "react";
 import { Context } from "../store/appContext.js";
-
+import { FaGithub } from "react-icons/fa";
+import "../../styles/nav.css"
+import "../../styles/global.css"
 
 export const Navbar = () => {
 	const {store, actions} = useContext(Context)
 
 	return (
-		<nav className="navbar navbar-light bg-light mb-3 container">
-			<Link to="/">
-				<span className="navbar-brand mb-0 h1">Star Wars</span>
+		<nav className="navbar navbar-light mb-3 py-5 navbar-edit border-gradient">
+			<Link to="https://github.com/4GeeksAcademy/StartWars-Flux" className="d-flex justify-content-center">
+				<FaGithub className="fs-3"/></Link>
+			<Link to="/" className="d-flex justify-content-center">
+				<img src="https://lumiere-a.akamaihd.net/v1/images/sw_logo_stacked_2x-52b4f6d33087_7ef430af.png?region=0,0,586,254" width={"150px"}/>
 			</Link>
 			<div className="ml-auto">
-				<div className="dropdown">
+				<div className="dropdown d-flex justify-content-center">
 					<button className="btn btn-secondary dropdown-toggle" type="button" id="dropdownMenuButton1" data-bs-toggle="dropdown" aria-expanded="false">
 						Favorites: <span>{store.favorites.length}</span>
 					</button>
